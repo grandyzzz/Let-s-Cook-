dac9e2276c1994647ff13ef029b91924ad18bd06
grandyzzz <twieank@gmail.com>
2016-09-24 12:18:29 +0300

    root

---
+++ app/src/main/java/com/kostya/letscook/RecyclerViewHolders.java
@@ -0,0 +1,24 @@
+package com.kostya.letscook;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RecyclerViewHolders extends RecyclerView.ViewHolder {
+
+    public TextView countryName;
+    public ImageView countryPhoto;
+    public FragmentTransaction tr;
+    public RecyclerViewHolders(View itemView) {
+        super(itemView);
+
+
+        countryName = (TextView)itemView.findViewById(R.id.country_name);
+        countryPhoto = (ImageView)itemView.findViewById(R.id.country_photo);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/kostya/letscook/WebFragment.java
@@ -0,0 +1,44 @@
+package com.kostya.letscook;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Костя on 29.05.2016.
+ */
+public class WebFragment extends Fragment {
+
+   private WebView webView;
+    static String URL;
+
+    public static WebFragment newInstance(String url){
+        WebFragment fragment = new WebFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("wURL", url);
+        URL=url;
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_web,container,false);
+
+        webView = (WebView) v.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(URL);
+
+
+        return v;
+    }
+
+
+}
